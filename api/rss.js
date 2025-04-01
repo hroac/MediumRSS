05c8be6d7c0521efc795fd306dbdfa83e0dbf6bd
hraoc <rsk.rook@gmail.com>
2025-04-01 10:43:46 +0200

    fix for cors

--- api/rss.js
+++ api/rss.js
@@ -1,7 +1,15 @@
-
-
 module.exports = async (req, res) => {
   try {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle preflight (OPTIONS) request
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+
     const rssUrl = 'https://medium.com/feed/@hraoc';
     const response = await fetch(rssUrl);
     const xml = await response.text();
